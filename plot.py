2606b2605e91b2c0d095ae57c1d7873c66e1c9e4
Broyojo <dha@xoba.com>
2021-12-04 08:10:16 -0500

    simplified projection code

--- plot.py
+++ plot.py
@@ -2,46 +2,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
+
 def load_words(path):
     with open(path, "r") as f:
         return json.loads(f.read())
+
 
 def project_words(words):
     vectors = np.array([words[word] for word in words])
     labels = [word for word in words]
 
-    u, s, v = np.linalg.svd(vectors)
-    vectors_2d = np.transpose(np.matmul(v[0:2,:], np.transpose(vectors)))
+    u, _, _ = np.linalg.svd(vectors)
+
+    # take first 2 dimensions of u since the numbers in s are decreasing
+    # we want to take the highest 2 dimensions which is the first two, as verified by s
+    vectors_2d = u[:, :2]
 
     return labels, vectors_2d
 
-def group_words(words, vectors_2d, labels, n_groupings = 4):
+
+def group_words(words, vectors_2d, labels, n_groupings=4):
     centers = [(np.random.rand(2) * 2 - 1) for _ in range(n_groupings)]
     groupings = [[] for _ in range(n_groupings)]
-    
-    for _ in range(10): #iterations
-        
+
+    for _ in range(10):  # iterations
+
         groupings = [[] for _ in range(n_groupings)]
-        
+
         for word in words:
             closest_group = 0
             closest_distance = 1e10
             for i, center in enumerate(centers):
-                distance = np.linalg.norm(np.subtract(center, vectors_2d[labels.index(word)]))
+                distance = np.linalg.norm(np.subtract(
+                    center, vectors_2d[labels.index(word)]))
                 if distance < closest_distance:
                     closest_distance = distance
                     closest_group = i
             groupings[closest_group].append(word)
-        
+
         for i, group in enumerate(groupings):
             vecs = []
             for word in group:
                 vecs.append(vectors_2d[labels.index(word)])
             vecs = np.array(vecs)
-            center = np.average(vecs, axis = 0)
+            center = np.average(vecs, axis=0)
             centers[i] = center
-    
+
     return groupings
+
 
 def plot(labels, vectors_2d, groupings):
     markers = "ovs*PXDdp."
@@ -52,33 +60,36 @@ def plot(labels, vectors_2d, groupings):
         for word in group:
             xs.append(vectors_2d[labels.index(word)][0])
             ys.append(vectors_2d[labels.index(word)][1])
-        plt.scatter(xs, ys, marker = markers[j])
+        plt.scatter(xs, ys, marker=markers[j])
 
         for i in range(len(group)):
             plt.annotate(
                 group[i],
-                (vectors_2d[labels.index(group[i])][0], vectors_2d[labels.index(group[i])][1]),
+                (vectors_2d[labels.index(group[i])][0],
+                 vectors_2d[labels.index(group[i])][1]),
                 textcoords="offset points",
-                xytext=(0,10),
+                xytext=(0, 10),
                 ha='center',
             )
 
     print([len(x) for x in groupings])
     plt.show()
-    
+
+
 def main():
     words = load_words("vectors.txt")
     labels, vectors_2d = project_words(words)
     grouping = None
     i = 0
     while not grouping and i < 1000000:
-        groupings = group_words(words, vectors_2d, labels, n_groupings=7)
-        if all([len(x)!=0 for x in groupings]):
+        groupings = group_words(words, vectors_2d, labels, n_groupings=4)
+        if all([len(x) != 0 for x in groupings]):
             grouping = groupings
         i += 1
         if i % 25 == 0:
             print(i)
     plot(labels, vectors_2d, grouping)
+
 
 if __name__ == "__main__":
     main()
